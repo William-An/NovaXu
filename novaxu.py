645838ed1d218586709853b1d34a3d984187ff0b
Weili An <China_Aisa@live.com>
2017-11-06 08:41:22 +0800

    add error msg

--- novaxu.py
+++ novaxu.py
@@ -3,7 +3,11 @@ import config
 cookies_string = config.cookies_string
 nova_url = config.nova_url
 cookies = dict([(k,g) for k,g in [j.split('=') for j in [i for i in cookies_string.split(';')]]])
-res = requests.post(nova_url, cookies=cookies)
-while res.status_code != 200:
+try:
 	res = requests.post(nova_url, cookies=cookies)
-print(res.json()['msg'])
+	while res.status_code != 200:
+		res = requests.post(nova_url, cookies=cookies)
+	print(res.json()['msg'])
+except:
+	print('续命失败')
+input()
